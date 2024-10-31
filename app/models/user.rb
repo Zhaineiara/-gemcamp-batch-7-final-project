2f3d285390569c067be1e72f05428530a3ecc5d0
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-10-31 16:29:53 +0800

    added phone validation and modified role column [feature 1]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :phone, phone: {
+    possible: true,
+    allow_blank: true,
+    types: %i[voip mobile],
+    countries: [:ph]
+  }
+
+  enum role: { client: 0, admin: 1 }
 end
