21b5024a6e6382709519d0d452d194de83ee056f
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-10-31 16:49:23 +0800

    added admin and client accounts in seed [feature 1]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: 'admin123@gmail.com', password: 'admin123', username: 'admin123', role: 'admin', phone: '+639995240842', coins: 0, total_deposit: 0.0, children_members: 0)
+User.create(email: 'client123@gmail.com', password: 'client123', username: 'client123', role: 'client', phone: '+639212514316', coins: 0, total_deposit: 0.0, children_members: 0)
