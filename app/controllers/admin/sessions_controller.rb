247ea58daa8f9422cb7c15a141183d12502df425
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-11-02 20:46:27 +0800

    added restriction for client access to admin sessions [feature 1]

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,9 +1,22 @@
 class Admin::SessionsController < Devise::SessionsController
-  def after_sign_in_path_for(resource)
-    admin_home_path
-  end
-
   def after_sign_out_path_for(resource_or_scope)
     new_admin_user_session_path
+  end
+
+  def create
+    user = User.find_by(email: params[:admin_user][:email])
+    if user && user.role == 'client'
+      flash[:alert] = "You are not authorized to access the admin panel."
+      redirect_to new_admin_user_session_path and return
+    end
+
+    if user&.valid_password?(params[:admin_user][:password])
+      set_flash_message!(:notice, :signed_in)
+      sign_in(resource_name, user)
+      redirect_to admin_home_path and return
+    else
+      flash[:alert] = "Invalid email or password."
+      redirect_to new_admin_user_session_path and return
+    end
   end
 end
