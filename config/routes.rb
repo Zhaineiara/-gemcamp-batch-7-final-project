a21325fbebd84aadf699287fdac751a5cf441dce
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-11-02 15:26:16 +0800

    added admin and client constraint [feature 2]

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,22 @@ Rails.application.routes.draw do
   get 'client/home',  to: 'client/home#dashboard'
   get 'admin/home/',  to: 'admin/home#dashboard'
 
-  namespace :admin do
-    devise_for :users, controllers: {
-      registrations: 'admin/registrations',
-      sessions: 'admin/sessions'
-    }
+  constraints(AdminDomainConstraint.new) do
+    namespace :admin do
+      devise_for :users, controllers: {
+        registrations: 'admin/registrations',
+        sessions: 'admin/sessions'
+      }
+    end
   end
 
-  namespace :client do
-    devise_for :users, controllers: {
-      registrations: 'client/registrations',
-      sessions: 'client/sessions'
-    }
+  constraints(ClientDomainConstraint.new) do
+    namespace :client do
+      devise_for :users, controllers: {
+        registrations: 'client/registrations',
+        sessions: 'client/sessions'
+      }
+    end
   end
 
 end
