81cb6825be435f7c4dcc03f2aecaa620bcbec215
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-11-04 19:11:38 +0800

    added edit profile and address button in client profile [feature 3]

--- app/controllers/client/registrations_controller.rb
+++ app/controllers/client/registrations_controller.rb
@@ -2,4 +2,8 @@ class Client::RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     client_home_path
   end
+
+  def after_update_path_for(resource)
+    client_profile_profile_path
+  end
 end
