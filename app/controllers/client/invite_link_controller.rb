cfe18b72a07f926034fa53e085864de580d8ff5d
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-11-12 13:37:10 +0800

    added qr invite link [feature 7]

--- app/controllers/client/invite_link_controller.rb
+++ app/controllers/client/invite_link_controller.rb
@@ -1,4 +1,23 @@
 class Client::InviteLinkController < ApplicationController
+  require "rqrcode"
   def index
+    @qrcode = RQRCode::QRCode.new(invitation_link)
+
+    # NOTE: showing with default options specified explicitly
+    @qrcode_svg = @qrcode.as_svg(
+      color: "000",
+      shape_rendering: "crispEdges",
+      module_size: 11,
+      standalone: true,
+      use_path: true
+    )
+  end
+
+  private
+
+  def invitation_link
+    "http://client.com:3000/client/users/sign_up?promoter=#{current_client_user}"
   end
 end
+
+
