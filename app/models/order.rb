7d5f07097afe8f3755e2f2a86fd9a48a29fca73a
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-12-02 18:25:19 +0800

    added aasm operation for order model [feature 20]

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,13 @@
 class Order < ApplicationRecord
-  before_create :generate_serial_number
+  include AASM
 
-  validates :offer_id, :state, :genre, presence: true
+  before_create :generate_serial_number, if: -> { serial_number.blank? }
+
+  validates :state, :genre, presence: true
+  validates :offer_id, presence: true, if: -> { genre == "deposit" }
   validates :serial_number, presence: true, uniqueness: true
-  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :amount, presence: true, if: -> { genre == "deposit" }
+  validates :amount, numericality: { greater_than: 0 }, if: -> { genre == "deposit" }
   validates :coin, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :remarks, length: { maximum: 500 }, allow_blank: true
 
@@ -12,8 +16,77 @@ class Order < ApplicationRecord
 
   enum genre: { deposit: 0, increase: 1, deduct: 2, bonus: 3, share: 4 }
 
+  aasm column: :state do
+    state :pending, initial: true
+    state :submitted, :cancelled, :paid
+
+    event :submit do
+      transitions from: :pending, to: :submitted
+    end
+
+    event :cancel do
+      transitions from: [:pending, :submitted], to: :cancelled
+      transitions from: :paid, to: :cancelled, after: :handle_cancelled_state
+    end
+
+    event :pay do
+      transitions from: :submitted, to: :paid, after: :handle_paid_state
+    end
+  end
+
+  private
   def generate_serial_number
     number_count = format('%04d', Order.where(user_id: user_id).count + 1)
     self.serial_number = "#{Time.current.strftime('%y%m%d')}-#{id}-#{user_id}-#{number_count}"
+  end
+  def increase_coins
+    user.update(coins: user.coins + coin)
+  end
+
+  def decrease_coins
+    if user.coins >= coin
+      user.update(coins: user.coins - coin)
+    else
+      errors.add(:base, "Not enough coins to deduct.")
+      raise ActiveRecord::Rollback
+    end
+  end
+
+  def decrease_coins_if_not_deduct
+    return if genre == "deduct"
+    decrease_coins
+  end
+
+  def increase_coins_if_deduct
+    return unless genre == "deduct"
+    increase_coins
+  end
+
+  def increase_total_deposit
+    return unless genre == "deposit"
+    user.update(total_deposit: user.total_deposit + amount)
+  end
+
+  def decrease_total_deposit_if_deposit
+    return unless genre == "deposit"
+    if user.total_deposit >= amount
+      user.update(total_deposit: user.total_deposit - amount)
+    else
+      errors.add(:base, "Not enough total deposit to deduct.")
+      raise ActiveRecord::Rollback
+    end
+
+  end
+
+  def handle_paid_state
+    increase_coins unless genre == "deduct"
+    decrease_coins if genre == "deduct"
+    increase_total_deposit
+  end
+
+  def handle_cancelled_state
+    decrease_coins_if_not_deduct
+    increase_coins_if_deduct
+    decrease_total_deposit_if_deposit
   end
 end
