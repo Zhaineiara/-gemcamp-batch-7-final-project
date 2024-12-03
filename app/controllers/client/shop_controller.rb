972404d7f537f0825f931103cc77f65cd0b380c6
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-12-03 23:14:19 +0800

    added order create for the offer [feature 21]

--- app/controllers/client/shop_controller.rb
+++ app/controllers/client/shop_controller.rb
@@ -1,11 +1,38 @@
 class Client::ShopController < ApplicationController
+  before_action :set_offer, only: :show
   layout 'client'
 
   def index
     @offers = Offer.where(status: 'active').order(:name)
   end
 
-  def show
+  def show;end
+
+  def create
+    offer = Offer.find(params[:offer_id])
+    order = Order.new(
+      user_id: current_client_user.id,
+      offer_id: offer.id,
+      genre: params[:genre],
+      remarks: params[:remarks],
+      state: 'pending',
+      amount: offer.amount,
+      coin: offer.coin
+    )
+
+    if order.save
+      flash[:notice] = "Order created successfully!"
+      redirect_to client_shop_path(offer)
+    else
+      flash[:alert] = order.errors.full_messages.to_sentence
+      redirect_to client_shop_path(offer)
+    end
+  end
+
+
+  private
+
+  def set_offer
     @offer = Offer.find(params[:id])
   end
 end
