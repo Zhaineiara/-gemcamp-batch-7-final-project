eacf84d348f890bf1f3b0cb9b4b543787956110a
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-12-11 19:22:12 +0800

    modified the client home and share page

--- app/controllers/client/home_controller.rb
+++ app/controllers/client/home_controller.rb
@@ -4,8 +4,8 @@ class Client::HomeController < ApplicationController
   def dashboard
     @banners = Banner.active.where("online_at <= ? AND offline_at > ?", Time.current, Time.current)
     @news_tickers = NewsTicker.active.limit(5)
-    @winners = Winner.shared.order(created_at: :desc).limit(5)
+    @winners = Winner.published.order(created_at: :desc).limit(5)
     @categories = Category.order(:name)
-    @items = Item.starting.active.limit(8).order(name: :asc)
+    @items = Item.starting.active.limit(8).order(created_at: :desc)
   end
 end
