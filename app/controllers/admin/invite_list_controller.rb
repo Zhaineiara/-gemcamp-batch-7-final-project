2323075cff8ccaf61560179c0f701ff98023ffa1
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-12-11 14:04:47 +0800

    added create CSV export button to invite list [feature 38]

--- app/controllers/admin/invite_list_controller.rb
+++ app/controllers/admin/invite_list_controller.rb
@@ -1,6 +1,7 @@
 class Admin::InviteListController < ApplicationController
   layout 'admin'
   before_action :authenticate_admin_user!
+  require 'csv'
 
   def index
     @users = User.includes(:parent)
@@ -16,5 +17,38 @@ class Admin::InviteListController < ApplicationController
     end
 
     @total_used_coins = @users.map { |user| [user.id, Ticket.where(user_id: user.id).sum(:coins)] }.to_h
+
+    respond_to do |format|
+      format.html
+      format.csv do
+        csv_string = CSV.generate(headers: true) do |csv|
+          csv << [
+            'Parent Email',
+            'User Email',
+            'Total Deposit',
+            'Member Total Deposit',
+            'Coins',
+            'Created At',
+            'Used Coins Count',
+            'Child Members'
+          ]
+
+          @users.each do |user|
+            csv << [
+              user.parent_email,
+              user.email,
+              user.total_deposit || 0,
+              user.children.sum(:total_deposit),
+              user.coins || 0,
+              user.created_at.strftime("%Y-%m-%d"),
+              @total_used_coins[user.id] || 0,
+              user.children_members || 0
+            ]
+          end
+        end
+
+        send_data csv_string, filename: "invite_list_#{Date.today}.csv"
+      end
+    end
   end
 end
